1cbf281b57726f29867093c016fb04209c88e7a9
Vladyslav <65622620+Naykitin@users.noreply.github.com>
2022-04-04 15:06:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ if(!isset($_SESSION["theme"]))
             <div class="wrapper">
                <div class="item" hover>
                   <img src="../img/med-master.png" alt="">
-                  <a href="/Med-master/index.php" target="_blank">Med-Master</a>
+                  <a href="/med-master/index.php" target="_blank">Med-Master</a>
                   <a href="https://github.com/Naykitin/Med-Master" target="_blank">GitHub</a>
                </div>
                <div class="item" hover>
